6ae096651cb85a2dfd39c681d2e527ad944315ec
Kaupo Juhkam <kaupo@credy.eu>
2016-08-24 16:33:54 +0300

    add autoresponder triggering

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -4,34 +4,54 @@ namespace nuffic\sendsmaily;
 
 use GuzzleHttp\Client;
 
-class ApiClient extends Configurable {
+class ApiClient extends Configurable
+{
 
     public $baseUrl;
     public $apiUser;
     public $apiPassword;
 
-    private $client;
+    private $_client;
 
-    public function getClient() {
-        if(!isset($this->client)) {
-            $this->client = new Client([
+    public function getClient()
+    {
+        if (!isset($this->_client)) {
+            $this->_client = new Client([
                 'base_uri' => $this->baseUrl,
                 'auth' => [$this->apiUser, $this->apiPassword]
             ]);
         }
-        return $this->client;
+        return $this->_client;
     }
 
-    public function addSubscriber($email, array $additionalParams = []) {
+    public function addSubscriber($email, array $additionalParams = [])
+    {
         $payload = [
-            'email'       => $email,
+            'email' => $email,
             'remote' => 1,
         ];
         $payload = array_merge_recursive($payload, $additionalParams);
         return json_decode($this->sendRequest('contact.php', $payload));
     }
 
-    private function sendRequest($resource, array $payload) {
+    public function triggerAutoresponder($responderId, $email, array $additionalParams = [])
+    {
+        $addresses = [
+            'email' => $email,
+        ];
+        $addresses = array_merge($addresses, $additionalParams);
+
+        $payload = [
+            'autoresponder' => $responderId,
+            'addresses' => [
+                $addresses
+            ],
+        ];
+        return json_decode($this->sendRequest('contact.php', $payload));
+    }
+
+    private function sendRequest($resource, array $payload)
+    {
         return $this->getClient()->post(
             $resource,
             ['form_params'    => $payload]
